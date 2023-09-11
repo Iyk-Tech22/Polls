081fbd52226a1c0dc3e1c57952256c99f8874e29
ikechukwugodwin22@yahoo.com <ikechukwugodwin22@yahoo.com>
2023-09-11 14:35:36 +0100

    Testing Poll Features

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# ADDING MORE CUSTOM TO OUR MODEL REPRS IN THE ADMIN
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionFieldAdmin(admin.ModelAdmin):
+    fields = ["question_text", "pub_date"]
+    inlines = [ChoiceInline]
+    
+    
+class QuestionFieldSetAdmin(admin.ModelAdmin):
+    fieldsets = [
+       (None, {"fields": ["question_text"]}),
+       ("Publish Date Information", {"fields": ["pub_date"], "classes":"collasped"})
+    ]
+    inlines = [ChoiceInline]
+
+    
+admin.site.register(Question, QuestionFieldAdmin)
+
